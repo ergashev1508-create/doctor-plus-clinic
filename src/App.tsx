ff1b1ca3ba509288eb8f4f2b0eb0a1134dfc3de9
Zhanat <ergashev.1508@gmail.com>
2026-05-13 01:21:40 -0700

    Remove auriculotherapy services

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ import { AdminLogin } from './components/Admin/AdminLogin';
 import { cn } from './lib/utils';
 import type { Doctor, Review } from './types';
 
-const CATEGORY_ORDER = ['Консультации', 'УЗИ', 'Гинекология', 'ЛОР', 'Эфферентология', 'Аурикулотерапия', 'Логопед-дефектолог', 'Процедурный кабинет', 'ЭКГ'];
+const CATEGORY_ORDER = ['Консультации', 'УЗИ', 'Гинекология', 'ЛОР', 'Эфферентология', 'Логопед-дефектолог', 'Процедурный кабинет', 'ЭКГ'];
 const CLINIC_MAP_URL =
   'https://2gis.kg/bishkek/search/%D1%83%D0%BB.%20%D0%9C%D0%B0%D1%85%D0%B0%D1%82%D0%BC%D1%8B%20%D0%93%D0%B0%D0%BD%D0%B4%D0%B8%2C%20201';
 const INSTAGRAM_URL = 'https://www.instagram.com/doctorplusclinic.kg/';
